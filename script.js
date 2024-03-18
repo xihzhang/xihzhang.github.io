0f452cb5a2ce9ca717f83cf6495168f7f65931b4
xihzhang <100363722+xihzhang@users.noreply.github.com>
2024-03-19 01:27:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 document.getElementById('loadData').addEventListener('click', function() {
-  fetch('https://login.usw2.pure.cloud/oauth/token')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      document.getElementById('apiResult').textContent = JSON.stringify(data, null, 2);
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
-    });
-});
+  const username = 'e5d897dc-717b-41e9-9b08-1d54934628a2';
+  const password = 'yovmWGmXubBTJLCbBRC2vI_WNwPSTJnDq2T9_jWgjI0';
+  const base64 = 'ZTVkODk3ZGMtNzE3Yi00MWU5LTliMDgtMWQ1NDkzNDYyOGEyOnlvdm1XR21YdWJCVEpMQ2JCUkMydklfV053UFNUSm5EcTJUOV9qV2dqSTA=';
+  const apiUrl = 'https://login.usw2.pure.cloud/oauth/token';
+  const data = {
+    key1: 'value1',
+    key2: 'value2'
+  };
+
+  fetch(apiUrl, {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Authorization': `Basic ${base64}`
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'grant_type': 'client_credentials'
+    
+  },
+  body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => console.log('Success:', data))
+  .catch((error) => console.error('Error:', error));
+  });
