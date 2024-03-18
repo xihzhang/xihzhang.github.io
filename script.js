1735557e3a54407c51f094fa8664087370f63fe9
xihzhang <100363722+xihzhang@users.noreply.github.com>
2024-03-19 02:09:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ document.addEventListener('DOMContentLoaded', function() {
   const fetchButton = document.getElementById('fetchButton');
 
   fetchButton.addEventListener('click', function() {
-  
-    const url = 'https://login.usw2.pure.cloud/oauth/token';
-    const headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'Basic ZTVkODk3ZGMtNzE3Yi00MWU5LTliMDgtMWQ1NDkzNDYyOGEyOnlvdm1XR21YdWJCVEpMQ2JCUkMydklfV053UFNUSm5EcTJUOV9qV2dqSTA='
-    };
-    const body = new URLSearchParams({
-    'grant_type': 'client_credentials'
+
+    fetch('https://login.usw2.pure.cloud/oauth/token', {
+      
+      method: 'POST', // cURL --location implies a GET by default, but --data-urlencode implies a POST
+      mode: 'no-cors', // can be 'cors', 'no-cors', or 'same-origin'
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ZTVkODk3ZGMtNzE3Yi00MWU5LTliMDgtMWQ1NDkzNDYyOGEyOnlvdm1XR21YdWJCVEpMQ2JCUkMydklfV053UFNUSm5EcTJUOV9qV2dqSTA='
+        },
+          body: 'grant_type=client_credentials' // Data from --data-urlencode
+        })
+      
+    .then(response => {
+      if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json(); // Assuming JSON response
+      })
+
+    .then(data => {
+      console.log(data); // Process the response data
+      })
+
+    .catch(error => {
+      console.error('Error:', error); // Handle any errors
     });
 
-  fetch(url, {
-    method: 'POST',
-    mode: 'no-cors', // can be 'cors', 'no-cors', or 'same-origin'
-    headers: headers,
-    body: body
-    })
-  
-    .then(response => {
-    if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-    })
-  
-    .then(data => {
-    console.log(data);
-    })
   });
 });
 
