f85bc2a38bba7efa8f66fa7120c88c772e3d22b8
xihzhang <100363722+xihzhang@users.noreply.github.com>
2024-03-19 01:47:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-document.getElementById('loadData').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
+  const fetchButton = document.getElementById('fetchButton');
+
+  fetchButton.addEventListener('click', function() {
   
-const url = 'https://login.usw2.pure.cloud/oauth/token';
-const headers = {
-  'Content-Type': 'application/x-www-form-urlencoded',
-  'Authorization': 'Basic ZTVkODk3ZGMtNzE3Yi00MWU5LTliMDgtMWQ1NDkzNDYyOGEyOnlvdm1XR21YdWJCVEpMQ2JCUkMydklfV053UFNUSm5EcTJUOV9qV2dqSTA='
-};
-const body = new URLSearchParams({
-  'grant_type': 'client_credentials'
+    const url = 'https://login.usw2.pure.cloud/oauth/token';
+    const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic ZTVkODk3ZGMtNzE3Yi00MWU5LTliMDgtMWQ1NDkzNDYyOGEyOnlvdm1XR21YdWJCVEpMQ2JCUkMydklfV053UFNUSm5EcTJUOV9qV2dqSTA='
+    };
+    const body = new URLSearchParams({
+    'grant_type': 'client_credentials'
+    });
+
+  fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: body
+    })
+  
+    .then(response => {
+    if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+    })
+  
+    .then(data => {
+    console.log(data);
+    })
+  });
 });
 
-fetch(url, {
-  method: 'POST',
-  headers: headers,
-  body: body
-})
-.then(response => {
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  return response.json();
-})
-.then(data => {
-  console.log(data);
-})
-.catch(error => {
-  console.error('Error:', error);
-});
